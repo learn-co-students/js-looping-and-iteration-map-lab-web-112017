bcddadc2946bc5c54321928d49b9be8f4d05aad1
mattfaircloth <mattcfaircloth@gmail.com>
2018-01-08 17:49:36 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution in this file.
+function lowerCaseDrivers(drivers) {
+  return drivers.map(function (name) {return name.toLowerCase()})
+}
+
+function nameToAttributes(drivers) {
+  return drivers.map(function (name) {
+    const begName = name.split(' ')[0];
+    const endName = name.split(' ')[1];
+    return {firstName: begName, lastName: endName}
+  })
+}
+
+function attributesToPhrase(drivers) {
+  return drivers.map(function (person) {
+    return `${person.name} is from ${person.hometown}`;
+  });
+}
