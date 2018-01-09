0cbdf5ab25af0098516cfc9d2a9b0d605988fd0e
Flatiron School <leslie.hsu@flatironschool.com>
2018-01-09 13:14:59 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solution in this file.
+function lowerCaseDrivers (array) {
+  //returns everything in lowercase
+  return array.map(function (driver) {return driver.toLowerCase()})
+}
+
+
+function nameToAttributes(driverNames) {
+  return driverNames.map(function (driverName) {
+    return {firstName: driverName.split(" ")[0], lastName: driverName.split(" ")[1]}}
+  )
+}
+
+function attributesToPhrase (driverObjs) {
+// returns a string saying `"<NAME OF DRIVER> is from <HOMETOWN>"`
+
+  return driverObjs.map(function (driverObj) {
+    return `${driverObj.name} is from ${driverObj.hometown}`
+  })
+}
