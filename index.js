fe2ef7c826e3804556ef34f6e65ae3913ce3031c
nspp99b <chromepanthers@gmail.com>
2018-01-08 21:46:03 -0500

    defined all functions

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution in this file.
+function lowerCaseDrivers(array) {
+  const newArray = array.map(function (driver) {
+    return driver.toLowerCase();
+  })
+  return newArray
+}
+
+function nameToAttributes(array) {
+  const newArray = array.map(function (driver) {
+    return Object.assign({}, { firstName: driver.split(" ")[0], lastName: driver.split(" ")[1] });
+  })
+  return newArray
+}
+
+function attributesToPhrase(array) {
+  const newArray = array.map(function (driver) {
+    return `${driver['name']} is from ${driver['hometown']}`
+  })
+  return newArray
+}
